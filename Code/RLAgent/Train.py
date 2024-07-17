7b23e99cb50c84508b9400a94d17092431efc015
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-17 05:04:07 -0400

    Update Train.py

--- Code/RLAgent/Train.py
+++ Code/RLAgent/Train.py
@@ -2,18 +2,16 @@ import json
 import logging
 
 def train_rl_agent(agent, env, train_loader, action_length, path, epoches=5):
-    RL_running_sum = 0
-    batch_num = 0
     for policy in range(epoches):
         logging.info(f'Policy: {policy}')
         total_step = 0
-
+        batch_num = 0
         print(f'Agent with epsilon = {agent.epsilon}')
         for batch in train_loader:
             batch_num += 1
-            batch_loss_sum = 0
             state = env.reset(batch)
 
+            batch_loss_sum = 0
             while (state["step"] < 512 - action_length):
                 actions, q_values = agent.action(policy, state)
 
@@ -26,11 +24,6 @@ def train_rl_agent(agent, env, train_loader, action_length, path, epoches=5):
                 batch_loss_sum += loss
                 total_step += action_length
 
-            RL_running_sum += batch_loss_sum
-            # print("Org Seq: ", batch['input_ids'])
-            # print("Enhanced Seq", state["selected_input_ids"])
-
-            logging.info(f'Running Mean RL Loss: {RL_running_sum / batch_num }')
-
+            logging.info(f'Batch avg RL Loss: {batch_loss_sum / batch_num }')
 
     return agent
